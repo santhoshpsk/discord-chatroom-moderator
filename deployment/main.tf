90673169bce1d4dc2cf14e26f04baba15bb550f9
Santhosh <santhoshpsk@outlook.com>
2024-08-22 10:56:22 +0530

    fix execution_role_arn

--- deployment/main.tf
+++ deployment/main.tf
@@ -35,6 +35,7 @@ resource "aws_ecs_task_definition" "ecs-main-task-definition" {
   requires_compatibilities = ["FARGATE"]
   network_mode             = "awsvpc"
   task_role_arn            = var.ecs-task-role-arn
+  execution_role_arn       = var.ecs-task-role-arn
 }
 
 resource "aws_ecs_service" "ecs-main-task-service" {
