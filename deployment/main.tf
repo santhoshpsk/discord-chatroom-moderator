c7d52ff59337cf773abf5010a47e85cc31f2782d
Santhosh <santhoshpsk@outlook.com>
2024-08-23 08:00:46 +0530

    decreasing task size again

--- deployment/main.tf
+++ deployment/main.tf
@@ -36,8 +36,8 @@ resource "aws_ecs_task_definition" "ecs-main-task-definition" {
       }
     }
   ])
-  cpu                      = 2048
-  memory                   = 4096
+  cpu                      = 256
+  memory                   = 512
   track_latest             = true
   requires_compatibilities = ["FARGATE"]
   network_mode             = "awsvpc"
